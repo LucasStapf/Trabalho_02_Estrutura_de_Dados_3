b1a19dd632280d73127734cdfa80b5f56b998b80
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-12-19 10:10:02 -0300

    Update main.c

---
+++ sources/main.c
@@ -0,0 +1,22 @@
+#include "../headers/commands.h"
+
+int main() {
+    createGraphCommand("C:\\Users\\lucas\\CLionProjects\\Trabalho_02___Estrutura_de_Dados_3\\binarios\\estacao1.bin");
+//    cmdSelector();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
